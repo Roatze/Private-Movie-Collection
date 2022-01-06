eaac0c241cb5345b7e79b257ce94fd7692f98a2c
MostafaHeidari <70130828+MostafaHeidari@users.noreply.github.com>
2022-01-06 11:22:33 +0100

    Select Movie blev lavet, sånden at man kan tilføje en mpeg4 og mp4 movie

--- src/gui/Controllers/AddMovieController.java
+++ src/gui/Controllers/AddMovieController.java
@@ -7,17 +7,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.SQLException;
 
 
-public class AddMovieController {
+public class AddMovieController{
+
+    @FXML
     public Button returnMainMenu;
+
+    @FXML
+    public TextField titleBar;
+
+    @FXML
+    public TextField personalRatngBar;
+
+    @FXML
+    public TextField imdbRaringBar;
+
+    @FXML
+    public Button movieSaveButton;
+
+    @FXML
+    public ComboBox genreMenu;
+
+    @FXML
+    public TextField fileBar;
 
     public void goReturnMainMenu(ActionEvent actionEvent) throws IOException {
         Stage swich = (Stage) returnMainMenu.getScene().getWindow();
@@ -30,5 +54,21 @@ public class AddMovieController {
     }
 
     public void chooseFile(ActionEvent actionEvent) {
+    }
+
+    public void chooseFileMethod(ActionEvent actionEvent) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Movie");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")+ System.getProperty("file.separator") +"Desktop"));
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Audio Files",  "*.mp4", "*.mpeg4", "*.aac"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            //insert data from file
+            fileBar.setText(selectedFile.getAbsolutePath());
+        }
     }
 }
