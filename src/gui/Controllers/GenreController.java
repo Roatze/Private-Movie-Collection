b2a5bb44b79e47c53600df8c74c4c2675526b94b
FrederikReigstad <Frederik9876@gmail.com>
2022-01-07 09:40:20 +0100

    lavet new controller til den nye fxml

---
+++ src/gui/Controllers/GenreController.java
@@ -0,0 +1,14 @@
+package gui.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class GenreController {
+    public void genreCancelBtn(ActionEvent event) {
+    }
+
+    public void genreSaveBtn(ActionEvent event) {
+    }
+
+    public void newGenreTxt(ActionEvent event) {
+    }
+}
